343e2d263b5ae8d66a8867539673080d44d0e48e
Luo-Leng <55864809+Luo-Leng@users.noreply.github.com>
2019-10-04 11:34:12 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,6 +3,63 @@
 //
 
 #include "test.h"
-void testReverse();
-void testSort();
-void testAdd();
+/*
+ * test MyArray::add()
+ */
+void testAdd() {
+    // create an object of MyArray
+    MyArray<int> arr(10);
+
+    // add values to the array
+    for (int i = 0; i< 5; i++) {
+        arr.add(i);
+    }
+
+    // check the size of the array
+    if (arr.count()==5) {
+        printf("testAdd: PASSED\n");
+    } else {
+        printf("testAdd: FAILED, expected:%d, actual:%d\n", 5, arr.count());
+    }
+}
+
+/*
+ * test MyArray::sort()
+ */
+void testSort() {
+    printf("testSort(): not implemented\n");
+}
+
+/* 
+ * test MyArray::reverse()
+ */
+void testReverse() {
+    printf("testReverse(): not implemented\n");
+}
+
+/*
+ * test MyArray::[]
+ */
+void testSubscriptOperator() {
+    // create an object of MyArray
+    MyArray<int> arr(10);
+
+    for (int i = 0; i<5; i++) {
+        arr[i] = i;
+    }
+
+    int testCount = 5, testPassed = 0;
+    for (int i=0; i<testCount; i++) {
+        if (arr[i]!=i) {
+            break;
+        } else {
+            testPassed++;
+        }
+    }
+
+    if (testPassed==testCount) {
+        printf("testSubscriptOperator: PASSED\n");
+    } else {
+        printf("testSubscriptOperator: FAILED\n");
+    }
+}
